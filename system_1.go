c1e182b859b9f9529561e03bb6fc15886ab77029
Nanana22mm <3110nanana2002@sslab.ics.keio.ac.jp>
2024-04-29 20:40:45 +0900

    show html and get input

--- system_1.go
+++ system_1.go
@@ -1,23 +1,102 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
+	"math"
 	"net/http"
+	"sort"
+
+	"github.com/gorilla/mux"
 )
 
+type ScheduleEntry struct {
+	Day        int
+	Event_name string
+	Start_time int
+	End_time   int
+	Memo       string
+	Record     string
+}
+
+type Schedule struct {
+	Id         int
+	Event_data *ScheduleEntry
+}
+
+type ScheduleList struct {
+	Schedule []*Schedule
+}
+
+const start_time_digit = 2
+
+// constructor and initializer of ScheduleEntry
+func NewScheduleEntry(day int, event_name string, start_time int, end_time int, memo string, record string) *ScheduleEntry {
+	s := new(ScheduleEntry)
+	s.Day = day
+	s.Event_name = event_name
+	s.Start_time = start_time
+	s.End_time = end_time
+	s.Memo = memo
+	s.Record = record
+	return s
+}
+
+func NewSchedule(id int, schedule_entry *ScheduleEntry) *Schedule {
+	s := new(Schedule)
+	s.Id = id
+	s.Event_data = schedule_entry
+	return s
+}
+
+func MakeId(day int, start_time int) int {
+	id := day*int(math.Pow10(start_time_digit)) + start_time
+	return id
+}
+
 func mainHandler(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("index.html")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+	tmpl := template.Must(template.ParseFiles("index.html"))
+
+	if r.Method == "POST" {
+		date := r.FormValue("date") // ex. 20240409
+		day := r.FormValue("day")   // mon, tue, wed, thr, fri
+		event_name := r.FormValue("event")
+		start_time := r.FormValue("start_time")
+		end_time := r.FormValue("end_time")
+		memo := r.FormValue("memo")
+		record := r.FormValue("record")
+
+		fmt.Printf("date: %s, day: %s, event_name: %s, start_time: %s, end_time: %s, memo: %s, record: %s\n", date, day, event_name, start_time, end_time, memo, record)
 	}
-	if err := t.Execute(w, nil); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+
+	tmpl.Execute(w, nil)
 }
 
 func main() {
-	http.HandleFunc("/", mainHandler)
-	http.ListenAndServe(":8000", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/", mainHandler)
+	http.Handle("/", r)
+	fmt.Println("boot server")
+	http.ListenAndServe(":8080", nil)
+
+	// get usr input
+
+	day := 20240429
+	event_name := "dev"
+	start_time := 1000
+	end_time := 1700
+	memo := "TODO"
+	record := ""
+
+	schedule_entry := NewScheduleEntry(day, event_name, start_time, end_time, memo, record)
+	schedule := NewSchedule(MakeId(day, start_time), schedule_entry)
+	schedule_list := &ScheduleList{}
+
+	scheduleSlice := schedule_list.Schedule
+	scheduleSlice = append(scheduleSlice, schedule)
+
+	// スケジュールをソート
+	sort.SliceStable(scheduleSlice, func(i, j int) bool {
+		return scheduleSlice[i].Id < scheduleSlice[j].Id
+	})
 }
