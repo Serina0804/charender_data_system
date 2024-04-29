b4699e7f5af8dabbbe4cb15cacc5506148899f82
Serina-0804 <miyake@ailab.ics.keio.ac.jp>
2024-04-29 14:34:44 +0900

    re_new

--- system_1.go
+++ system_1.go
@@ -7,17 +7,12 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "index.html")
-}
-
 func RegisterHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", RegisterHandler)
-	r.HandleFunc("/index.html", IndexHandler) // 追加
 
 	http.Handle("/", r)
 
