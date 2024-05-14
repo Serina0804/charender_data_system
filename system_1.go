ac8bfe642955ce678f61de75d34c905148074531
Nanana22mm <3110nanana2002@sslab.ics.keio.ac.jp>
2024-05-14 18:31:12 +0900

    able to add entry into table

--- system_1.go
+++ system_1.go
@@ -10,43 +10,19 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type ScheduleEntry struct {
-	Date       int
-	Day        string
-	Event_name string
-	Start_time int
-	End_time   int
-	Memo       string
-	Record     string
-}
-
 type Schedule struct {
-	Id         int
-	Event_data *ScheduleEntry
+	Id        int
+	Date      int
+	Day       string
+	EventName string
+	StartTime int
+	EndTime   int
+	Memo      string
+	Record    string
 }
 
-// constructor and initializer of ScheduleEntry
-func NewScheduleEntry(date int, day string, event_name string, start_time int, end_time int, memo string, record string) *ScheduleEntry {
-	s := new(ScheduleEntry)
-	s.Date = date
-	s.Day = day
-	s.Event_name = event_name
-	s.Start_time = start_time
-	s.End_time = end_time
-	s.Memo = memo
-	s.Record = record
-	return s
-}
-
-func NewSchedule(id int, schedule_entry *ScheduleEntry) *Schedule {
-	s := new(Schedule)
-	s.Id = id
-	s.Event_data = schedule_entry
-	return s
-}
-
-func MakeId(str_date string, str_start_time string) int {
-	str_id := str_date + str_start_time
+func MakeId(date int, start_time int) int {
+	str_id := strconv.Itoa(date) + strconv.Itoa(start_time)
 
 	id, err := strconv.Atoi(str_id)
 	if err != nil {
@@ -85,12 +61,17 @@ func mainHandler(w http.ResponseWriter, r *http.Request) {
 			fmt.Printf("end_time must be number\n")
 		}
 
-		// make schedule entry
-		schedule_entry := NewScheduleEntry(date, day, event_name, start_time, end_time, memo, record)
-		schedule := NewSchedule(MakeId(str_date, str_start_time), schedule_entry)
+		scheduleList = append(scheduleList, &Schedule{
+			Id:        MakeId(date, start_time),
+			Date:      date,
+			Day:       day,
+			EventName: event_name,
+			StartTime: start_time,
+			EndTime:   end_time,
+			Memo:      memo,
+			Record:    record,
+		})
 
-		// append list
-		scheduleList = append(scheduleList, schedule)
 		sort.SliceStable(scheduleList, func(i, j int) bool {
 			return scheduleList[i].Id < scheduleList[j].Id
 		})
@@ -102,17 +83,13 @@ func mainHandler(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	tmpl.Execute(w, nil)
-}
-func acceptHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("accept.html"))
-	tmpl.Execute(w, nil)
+	tmpl.Execute(w, scheduleList)
 }
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", mainHandler)
-	r.HandleFunc("/accept", acceptHandler)
+	// r.HandleFunc("/accept", acceptHandler)
 	http.Handle("/", r)
 	fmt.Println("boot server")
 	http.ListenAndServe(":8080", nil)
